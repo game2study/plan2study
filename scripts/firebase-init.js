00e7cdf4d826b58c6619500e18e3764ad5fd477b
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-09-15 18:41:29 -0700

    attempted to fix firebase auth

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
     apiKey: "AIzaSyAOTD04xQC3NPSp6_WiXBuTJR2D6YPA2ac",
     authDomain: "game2study.firebaseapp.com",
@@ -18,7 +15,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = firebase.auth();
 var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().useDeviceLanguage();
