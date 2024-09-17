9ae2aff67f064afb360567f2625e3b361f5a046d
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-09-17 12:13:10 -0700

    moved signup script to its own seperate little hidey hole

---
+++ scripts/signup.js
@@ -0,0 +1,11 @@
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
