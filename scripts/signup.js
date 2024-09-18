6f988fc560fff4e2ce6acd612d0eeeff0430afb8
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-09-17 20:22:06 -0700

    looks like i made a teeny tiny mistake.. oh well, time to nuke servers! Yay!!!

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,7 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js'
 const auth = getAuth()
 //and if this doesn't work, someone isn't going to see the light of day
-createUserWithEmailAndPassword(auth, email, password)
+createUserWithEmailAndPassword(auth, "bobisangry@example.com", "runForY0urL1f3@5B0B15ANGRY")
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
