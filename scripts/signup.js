7ef5e754adf929e8c151bbcfd5be38e778e9774e
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-09-17 20:41:31 -0700

    made sign up button do something

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,16 +1,17 @@
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js'
 const auth = getAuth()
 //and if this doesn't work, someone isn't going to see the light of day
-createUserWithEmailAndPassword(auth, "bobisangry@example.com", "5B0B15ANGRY")
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode)
-    console.error(errorMessage)
-    // ..
-  });
+document.getElementById("submitSignup").addEventListener("click", () => {
+    createUserWithEmailAndPassword(auth, document.getElementById("emailField").value, "5B0B15ANGRY")
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage)
+        // ..
+      });
+});
