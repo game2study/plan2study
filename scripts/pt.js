fece5e5b23e7c2485a27d4218ea311722650d79c
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-10-20 07:44:41 -0700

    finished workspace design, starting func

---
+++ scripts/pt.js
@@ -0,0 +1,21 @@
+var workspace = {
+    "name":"Homework",
+    "items": [
+
+    ]
+}
+/* Item Template:
+let item = {
+    "label":"Subject",
+    "type":"dropdown"
+    "extra":[
+        "Social Studies",
+        "Science",
+        "Math",
+        "Spanish",
+        "Band",
+        "English"
+    ]
+}
+*/
+console.log(document.querySelectorAll("pt_item"));
