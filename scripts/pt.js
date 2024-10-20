6aee117a8ac953f162fe3369fcbd78be7fd33915
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-10-20 08:01:38 -0700

    added a populateSidebar for ptw

--- scripts/pt.js
+++ scripts/pt.js
@@ -18,4 +18,26 @@ let item = {
     ]
 }
 */
-console.log(document.querySelectorAll("pt_item"));
+function populateSidebar(inputs){
+    inputs.forEach((input) => {
+        let workspaceElement = document.getElementById("pt_workspace");
+        let element = document.createElement("div");
+        let nameElement = document.createElement("div");
+        let contElement = document.createElement("div");
+        let svgDown = document.createElement("img");
+        let svgUp = document.createElement("img");
+        svgUp.src = "/assets/arrowup.svg";
+        svgDown.src = "/assets/arrowdown.svg";
+        svgUp.className = "ptic_up";
+        svgDown.className = "ptic_down";
+        element.className = "pt_item";
+        nameElement.className = "pt_item_name";
+        contElement.className = "pt_item_controls";
+        nameElement.innerText = input.type;
+        contElement.append(svgUp);
+        contElement.append(svgDown);
+        element.append(nameElement);
+        element.append(contElement);
+        workspaceElement.append(element);
+    })
+}
