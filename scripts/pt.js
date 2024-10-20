85c442e870be41dca340115cc673c4d9da2499b6
spacegaming631 <123908053+sg631@users.noreply.github.com>
2024-10-20 08:10:57 -0700

    addedsidebar config

--- scripts/pt.js
+++ scripts/pt.js
@@ -4,6 +4,10 @@ var workspace = {
 
     ]
 }
+const sidebarConfig = [
+    {"type":"Name"},
+    {"type":"YOO"}
+]
 /* Item Template:
 let item = {
     "label":"Subject",
@@ -20,7 +24,7 @@ let item = {
 */
 function populateSidebar(inputs){
     inputs.forEach((input) => {
-        let workspaceElement = document.getElementById("pt_workspace");
+        let sidebarElement = document.querySelector(".sidebar");
         let element = document.createElement("div");
         let nameElement = document.createElement("div");
         let contElement = document.createElement("div");
@@ -38,6 +42,29 @@ function populateSidebar(inputs){
         contElement.append(svgDown);
         element.append(nameElement);
         element.append(contElement);
-        workspaceElement.append(element);
+        sidebarElement.append(element);
     })
 }
+function addWorkspaceElem(input){
+    let workspaceElement = document.getElementById("pt_workspace");
+    let element = document.createElement("div");
+    let nameElement = document.createElement("div");
+    let contElement = document.createElement("div");
+    let svgDown = document.createElement("img");
+    let svgUp = document.createElement("img");
+    svgUp.src = "/assets/arrowup.svg";
+    svgDown.src = "/assets/arrowdown.svg";
+    svgUp.className = "ptic_up";
+    svgDown.className = "ptic_down";
+    element.className = "pt_item";
+    nameElement.className = "pt_item_name";
+    contElement.className = "pt_item_controls";
+    nameElement.innerText = input.type;
+    contElement.append(svgUp);
+    contElement.append(svgDown);
+    element.append(nameElement);
+    element.append(contElement);
+    workspaceElement.append(element);
+}
+
+populateSidebar(sidebarConfig);
